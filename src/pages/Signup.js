8cd2f141ab448f42c7ca7028c56db3db5991e4d4
rajindra19524 <rajindraprasan@gmail.com>
2024-12-20 12:52:40 +0530

    error in firebase auth

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,14 +1,18 @@
 import styles from './Signup.module.css'
 import { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 function Signup(){
         const [email,setEmail] = useState('')
         const [password,setPassword] = useState('')
         const [displayName,setDisplayName] = useState('')
+        const {signup,error,isPending} = useSignup()
+        
 
         const handleSubmit = (e) =>{
             e.preventDefault()
             console.log(email,password,displayName)
+            signup(email,password,displayName)
         }
 
     return(
@@ -38,7 +42,9 @@ function Signup(){
                         />
                     </label>
         
-                    <button className='btn'>Signup</button>
+                   {!isPending && <button className='btn'>Signup</button>}
+                    {isPending && <button className='btn' disabled>Loading</button>}
+                    {error && <p>{error}</p>}
                 </form>
     )
 }
